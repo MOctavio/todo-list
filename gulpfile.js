7548985632ac28703ca46ae6d4980ff965bb0955
Mario Octavio <jimenez303@gmail.com>
2016-03-12 10:45:13 -0600

    Gulp modifications for future implementation

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,54 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
+var htmlhint = require('gulp-htmlhint');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
+var minifyCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var es = require('event-stream');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  partials: ['./www/js/extensions/directives/**/_*.html'],
+  dest: ['./www/']
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+var pipes = {};
+pipes.validatedPartials = function() {
+  return gulp.src(paths.partials)
+    .pipe(htmlhint({
+      'doctype-first': false
+    }))
+    .pipe(htmlhint.reporter());
+};
+// pipes.builtPartials = function() {
+//   return pipes.validatedPartials()
+//     .pipe(gulp.dest(paths.dest+'views/'));
+// };
+pipes.builtStyles = function() {
+  return gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.dest + 'css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(gulp.dest('./www/css/'));
+};
+pipes.builtApp = function() {
+  return es.merge(pipes.validatedPartials(), pipes.builtStyles());
+};
+
+gulp.task('default', pipes.builtApp);
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.sass, function() {
+    return pipes.builtStyles();
+  });
 });
 
 gulp.task('install', ['git-check'], function() {
